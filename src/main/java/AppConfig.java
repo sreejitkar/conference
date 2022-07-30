7bb1d8a67dfe9db1ab2335aac0f025636b294a47
sreejitkar <sreejitkar999@gmail.com>
2022-07-31 03:00:18 +0530

    Tested Singleton class usage

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,5 +1,7 @@
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
@@ -10,9 +12,8 @@ import com.pluralsight.service.SpeakerServiceImpl;
 public class AppConfig {
 
 	@Bean(name = "speakerService")
+	@Scope(value = BeanDefinition.SCOPE_SINGLETON)
 	public SpeakerService getSpeakerService() {
-		// SpeakerServiceImpl service = new SpeakerServiceImpl();
-		// service.setRepository(getSpeakerRepository());
 
 		SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
 		return service;
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,11 +8,13 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-		// SpeakerService service = new SpeakerServiceImpl();
-
 		SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+		System.out.println(service);
 
 		System.out.println(service.findAll().get(0).getFirstName() + service.findAll().get(0).getLastName());
+
+		SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+		System.out.println(service2);
 
 	}
 
